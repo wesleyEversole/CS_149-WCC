6e1640bf96f8c5e88e4ce932c175d8cdc813f2ab
ktapakorn <bossmonfire@gmail.com>
2014-02-19 19:03:00 -0800

    preemptive hpf

---
+++ Assignment 2/src/HPF_preemptive.java
@@ -0,0 +1,21 @@
+public class HPF_preemptive implements QueInterface {
+
+	@Override
+	public void add(Process p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Process next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
