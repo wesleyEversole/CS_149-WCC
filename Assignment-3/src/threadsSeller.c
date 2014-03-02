1ffbe12ef989e8da44763332c908ce26dff1a9bc
wesleyEvesole <wesley.ever@gmail.com>
2014-03-02 15:13:46 -0800

    fixed bugs in person/seller methods. extended self tests.

--- Assignment-3/src/threadsSeller.c
+++ Assignment-3/src/threadsSeller.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <assert.h>
 
 #define LOCK(l) pthread_mutex_lock((l))
 #define UNLOCK(l) pthread_mutex_unlock((l))
@@ -65,7 +66,7 @@ Seller *allSellers[NUM_SELLERS];
 Seller *createSeller(Price p, int id) {
 	Seller *s;
 	s = malloc(sizeof(Seller));
-	s->lock = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	s->lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
 	s->id = id;
 	s->price = p;
 	s->que = NULL;
@@ -87,13 +88,17 @@ Seller *createSeller(Price p, int id) {
 	}
 	return s;
 }
-
-Person *createPerson(int sellerNum) {
+/**
+ * createPerson()
+ *   create a person and assign to the seller passed as an argument
+ */
+Person *createPerson(Seller *s) {
 	Person *p;
 	p = malloc(sizeof(Person));
 	p = mkSelfReferential(p);
 	p->inQ = FALSE;
-	// assign to a seller???
+	p->seller = s;
+	p->arrival = rand() % 60;
 	return p;
 }
 // general utility methods
@@ -111,7 +116,7 @@ void queAdd(Seller *s, Person *person) {
 	person->id = s->pid++;
 
 	if (s->que == NULL) {
-		person=mkSelfReferential(person) ;// should already be true
+		person = mkSelfReferential(person); // should already be true
 		s->que = person;
 		s->tail = person;
 	} else {
@@ -127,13 +132,13 @@ Person *mkSelfReferential(Person* p) {
 	return p;
 }
 
-Person *queRemove(Seller *s,Person *que) {
+Person *queRemove(Seller *s, Person *que) {
 	Person *p;
 	if (que == NULL) {
 		return NULL;
 	} else {
 		p = que;
-		if (p->next == que && p->prev==que) {
+		if (p->next == que && p->prev == que) {
 			// only one person on queue
 			// que is now empty
 			s->que = NULL;
@@ -141,12 +146,16 @@ Person *queRemove(Seller *s,Person *que) {
 			// tail removal
 			que->prev->next = que->prev;
 		} else {
+			// middle form?
 			que->next->prev = que->prev;
 			if (p->prev == que) {
 				// head removal (normal case)
 				s->que = p->next;
+				p->next->prev = p->next;
 			} else {
-				que->prev = que->next;
+				// remove from the middle
+				que->prev->next = que->next;
+				que->next->prev =  que->prev;
 			}
 		}
 	}
@@ -154,17 +163,17 @@ Person *queRemove(Seller *s,Person *que) {
 	p->inQ = FALSE;
 	return p;
 }
-void *addPerson(Person *person) {
+void addPerson(Person *person) {
 	sleep(person->arrival);
 	LOCK(person->seller->lock);
 	person->inQ = TRUE;
 	queAdd(person->seller, person);
 	UNLOCK(person->seller->lock);
 	// can explicitly kill thread if we want
-	return NULL;
 }
 
 Person *removePerson(Person *person) {
+	if (person==NULL) return NULL;
 	LOCK(person->seller->lock);
 	person = queRemove(person->seller, person);
 	UNLOCK(person->seller->lock);
@@ -190,35 +199,52 @@ void getLowSeat(Concert *hall, Person *p) {
 }
 
 void getMediumSeat(Concert *hall, Person *p) {
-   // act like high or low depending on set point
+	// act like high or low depending on set point
 	int row = p->seller->lastRow;
-    while (row>0 && row<=ROWS && isRowFull(hall,row)) {
-	   switch (row) {
-	   case 5: row = 6; break;
-	   case 6: row = 4;  break;
-	   case 4:row = 7;break;
-	   case 7: row=3; break;
-	   case 3: row=8; break;
-	   case 8: row=2;  break;
-	   case 2: row=9; break;
-	   case 9: row=1; break;
-	   case 1: row=10; break;
-	   default: // something wrong
-		   row = 0;
-		   break;
-	   }
-   }
-   if  (row >0 && row <= ROWS) {
-	   // legal row to check for seat
-	   p->seller->lastRow = row;
-	   if (row <=5) {
-		   getLowSeat(hall,p);
-	   } else {
-		   getHighSeat(hall,p);
-	   }
-   }
+	while (row > 0 && row <= ROWS && isRowFull(hall, row)) {
+		switch (row) {
+		case 5:
+			row = 6;
+			break;
+		case 6:
+			row = 4;
+			break;
+		case 4:
+			row = 7;
+			break;
+		case 7:
+			row = 3;
+			break;
+		case 3:
+			row = 8;
+			break;
+		case 8:
+			row = 2;
+			break;
+		case 2:
+			row = 9;
+			break;
+		case 9:
+			row = 1;
+			break;
+		case 1:
+			row = 10;
+			break;
+		default: // something wrong
+			row = 0;
+			break;
+		}
+	}
+	if (row > 0 && row <= ROWS) {
+		// legal row to check for seat
+		p->seller->lastRow = row;
+		if (row <= 5) {
+			getLowSeat(hall, p);
+		} else {
+			getHighSeat(hall, p);
+		}
+	}
 }
-
 
 void getSeat(Concert *hall, Person *person, Price p) {
 	LOCK(hall->lock);
@@ -240,29 +266,38 @@ void getSeat(Concert *hall, Person *person, Price p) {
 	UNLOCK(hall->lock);
 }
 
-int getRandomTime(int low,int high) {
+int getRandomTime(int low, int high) {
 	// create random number in range
 	//   (rnd()*(high-low))+low
 	return high;
 }
 void sellTickets(Seller *s) {
 	// threaded method for seller
-    Person *p;
-    int tl,th;
+	Person *p;
+	int tl, th;
 
 	while (!(hall.hasStarted || hall.isSoldOut)) {
 		if (s->que != NULL) {
-			p=removePerson(s->que);
-			getSeat(&hall,p,s->price);
+			p = removePerson(s->que);
+			getSeat(&hall, p, s->price);
 			switch (s->price) {
-			case HIGH: tl=1;th=2;  break;
-			case MEDIUM: tl=2;th=4; break;
-			case LOW: tl=4;th=7; break;
+			case HIGH:
+				tl = 1;
+				th = 2;
+				break;
+			case MEDIUM:
+				tl = 2;
+				th = 4;
+				break;
+			case LOW:
+				tl = 4;
+				th = 7;
+				break;
 			default: // broken code
-				printf("Error, we've been hacked Price is %d",s->price);
+				printf("Error, we've been hacked Price is %d", s->price);
 				exit(-1);
 			}
-			sleep(getRandomTime(tl,th));// random time based on price
+			sleep(getRandomTime(tl, th)); // random time based on price
 			// this code needed to handle frustrated customers safely
 			LOCK(garbage->lock);
 			queAdd(garbage, p);
@@ -275,8 +310,83 @@ void sellTickets(Seller *s) {
 void doorsCloseIn60Minutes() {
 	sleep(60);
 }
+Boolean checkN(int value, int expect, char * msg) {
+	if (expect != value) {
+		printf("%s: Found %d, Expected %d\n", msg, value, expect);
+	}
+}
+Boolean checkT(int expr, char * msg) {
+	if (!expr) {
+		printf("%s: returned FALSE %d\n", msg, expr);
+	}
+	return expr;
+}
 void mkTest() {
-	printf("Self Test\n");
+
+	printf(" Seller\n");
+	srand(0);
+	Seller *s;
+	s = createSeller(HIGH, 1);
+	assert(checkT(s->price==HIGH,"Seller is HIGH"));
+	assert(checkT(s->que==NULL,"Seller starts with empty que"));
+	assert(checkN(s->lastRow,1,"HIGH seller starts in row 1"));
+	free(s);
+	s = createSeller(MEDIUM, 1);
+	assert(checkN(s->lastRow,5,"MEDIUM seller starts in row 5"));
+	free(s);
+	s = createSeller(LOW, 1);
+	assert(checkN(s->lastRow,10,"LOW seller starts in row 10"));
+
+	printf(" Person\n");
+	Person *p = createPerson(s);
+	assert(checkN(p->arrival,43,"p->arrival"));
+	assert(checkT(s==p->seller,"checkT"));
+	assert(checkT((p->next==p->prev) && (p->prev==p),"p is self referential"));
+
+	// add person to seller
+	//   we need 4 people to test que management
+	//   for our testing set arrival to 0 and do not thread
+	printf(" Add Person\n");
+	int i;
+	for(i=0; i<4;i++) {
+		p=createPerson(s);
+		p->arrival = 0;
+		addPerson(p);
+	}
+	assert(checkN(s->que->id,1,"First customer id"));
+	assert(checkN(s->que->next->id,2,"Second customer id"));
+	assert(checkN(s->que->next->next->id,3,"Third customer id"));
+	assert(checkN(s->que->next->next->next->id,4,"Fourth/Last customer id"));
+	assert(checkN(s->que->next->next->next->next->id,4,"Fifth customer id (tail points to self"));
+
+	assert(checkT(s->que->inQ==TRUE,"First customer id"));
+	assert(checkT(s->que->next->inQ==TRUE,"Second customer id"));
+	assert(checkT(s->que->next->next->inQ==TRUE,"Third customer id"));
+	assert(checkT(s->que->next->next->next->inQ==TRUE,"Fourth/Last customer id"));
+
+	printf(" Remove Person\n");
+	// test removals
+	p = removePerson(s->que);// remove person 1
+	assert(checkN(p->id,1,"First person on que removed"));
+	assert(checkN(s->que->id,2,"First person on que now number 2"));
+
+	p = removePerson(s->que->next); // remove person 3
+	assert(checkN(p->id,3,"Second(middle) person on que removed"));
+	assert(checkN(s->que->id,2,"First person on que now number 2"));
+	assert(checkN(s->que->next->id,4,"Second person should now be #4"));
+
+	p = removePerson(s->que->next); // remove person 4
+	assert(checkN(p->id,4,"Second(tail) person on que removed"));
+	assert(checkN(s->que->id,2,"First person on que now number 2"));
+	assert(checkN(s->que->id,s->que->next->id,"Only one person left on que #2"));
+	assert(checkT((p->next==p->prev) && (p->prev==p),"p(2) is self referential"));
+
+	p = removePerson(s->que);// remove person 2 (single person left)
+	assert(checkN(p->id,2,"Last person on que removed"));
+	assert(checkT(s->que==NULL,"Seller que now empty"));
+
+	p = removePerson(s->que);// remove person from empty que
+	assert(checkT(p==NULL,"NULL pointer expected"));
 }
 
 main(int argc, char *argv[]) {
@@ -286,30 +396,32 @@ main(int argc, char *argv[]) {
 	//   we have to wait on possible frustrated users
 	//   threads have cleared or they may crash when their
 	//   data is freed
-	garbage = createSeller(HIGH,0);
+	garbage = createSeller(HIGH, 0);
 	// finish creation of the concert hall
-	hall.isSoldOut=FALSE;
-	hall.hasStarted=FALSE;
-	hall.lock = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	hall.isSoldOut = FALSE;
+	hall.hasStarted = FALSE;
+	hall.lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
 
 	if (argc == 2) {
-		if (argv[1][0]=='T') {
+		if (argv[1][0] == 'T') {
 			// do self test
+			printf("Self Test\n");
 			mkTest();
+			printf("Self test succeeded\n");
 			exit(0);
 		}
 		int N = atoi(argv[1]);
-		printf("Run with %d customers per ticket seller\n",N);
-	// create sellers
-	Seller *allSellers[NUM_SELLERS];
+		printf("Run with %d customers per ticket seller\n", N);
+		// create sellers
+		Seller *allSellers[NUM_SELLERS];
 
-	// create attendees
+		// create attendees
 
-	Person *p;
-	p = (Person *) malloc(sizeof(Person));
+		Person *p;
+		p = (Person *) malloc(sizeof(Person));
 
-	//pthread_create(pthread_t *thread_id, const pthread_attr_t *attributes,
-	//        void *(*thread_function)(void *), void *arguments);
+		//pthread_create(pthread_t *thread_id, const pthread_attr_t *attributes,
+		//        void *(*thread_function)(void *), void *arguments);
 	} else {
 		printf("Usage:\n");
 		printf("threadSeller [T|number]\n");
