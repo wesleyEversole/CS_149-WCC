9f0527766037845695f913675fb278c135c3e98d
ktapakorn <bossmonfire@gmail.com>
2014-03-20 23:27:12 -0700

    attempt at next fit

--- Assignment-4/src/NextFitSwap.java
+++ Assignment-4/src/NextFitSwap.java
@@ -1,14 +1,35 @@
-
 public class NextFitSwap extends BaseSwapper {
+
+	private int lastlocation;
+
 	public NextFitSwap(Memory m) {
 		super(m);
-		// TODO Auto-generated constructor stub
+		lastlocation = 0;
 	}
+
 	@Override
 	public Boolean load(Process p) {
-		super.load(p);
+		int processSize = p.getSize();
+		if (isMemoryAvailable(processSize)) {
+			MemoryBlock mb;
+
+			for (int blockNum = lastlocation; blockNum < free.size(); blockNum++) {
+				mb = free.get(blockNum);
+				if (processSize <=mb.getSize()) {
+					MemoryBlock pmb = new MemoryBlock(mb.getStart(), processSize);
+					allocated.add(pmb);
+					mb.setStart(pmb.getEnd()+1);
+					mb.setSize(mb.getSize()-processSize);
+				}
+
+			}
+			if (lastlocation == free.size()) {
+				lastlocation = 0;
+			}
+		}
 		return false;
 	}
+
 	@Override
 	public String name() {
 		return "Next Fit";
