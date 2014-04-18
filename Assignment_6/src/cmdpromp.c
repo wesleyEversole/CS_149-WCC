52c3976116a5d559e1fea3dcd7a0cb5c51ff71dc
ktapakorn <bossmonfire@gmail.com>
2014-04-18 14:07:13 -0700

    input method is done, Im not sure how you want to structure this. the write part I think I’m supposed to write pipe and not an actual file. I’m still working on that.

--- Assignment_6/src/cmdpromp.c
+++ Assignment_6/src/cmdpromp.c
@@ -1,1 +1,27 @@
-//this file should have a funtion that will print to the cmd and take a message.
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+
+
+	//child process 5
+	if (pid[4] == 0)
+	{
+		//close read pipe
+
+
+
+		//write to pipe
+
+	}
+
+}
+
+char* getInput ()
+{
+	char buf[128];
+	printf("Enter a Message: ");
+	char *input = fgets(buf, 128, stdin);
+	getchar();
+	return input;
+}
